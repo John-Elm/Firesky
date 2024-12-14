d20c19129bc3779aaca619b0d3d76ed708d2a35b
Johnny Curran <18315178+JohnnyCurran@users.noreply.github.com>
2024-12-13 21:53:56 -0600

    Bump Limit to 25

--- lib/firesky_web/live/home_live.ex
+++ lib/firesky_web/live/home_live.ex
@@ -25,7 +25,7 @@ defmodule FireskyWeb.HomeLive do
     socket =
       socket
       |> stream_configure(:frames, dom_id: &elem(&1, 0))
-      |> stream(:frames, [], limit: 100)
+      |> stream(:frames, [], limit: 25)
 
     {:ok, socket}
   end
@@ -33,11 +33,12 @@ defmodule FireskyWeb.HomeLive do
   @impl Phoenix.LiveView
   def handle_info({:frame, json_map}, socket) do
     id = json_map["commit"]["cid"]
-    json =
-      json_map
-      |> Jason.encode!()
-      |> Jason.Formatter.pretty_print()
-    socket = stream_insert(socket, :frames, {id, json}, limit: -10, at: 0)
+    json = inspect(json_map)
+      # json_map
+      # |> Jason.encode!()
+      # |> Jason.Formatter.pretty_print()
+
+    socket = stream_insert(socket, :frames, {id, json}, limit: 25, at: 0)
     {:noreply, socket}
   end
 end
